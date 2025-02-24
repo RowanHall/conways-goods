4ded5f0e20f44ffea00f165a9907396899981fc6
RowanHall <rowancedarhall@gmail.com>
2025-02-23 23:16:07 -0800

    config changes

--- backend/app/utils/db.py
+++ backend/app/utils/db.py
@@ -1,11 +1,11 @@
 import psycopg2
-from config import DevelopmentConfig
+from config import ProductionConfig as Config
 
 def get_db_connection(): #TODO: move to utils.py #MOVING: TO UTILS.PY
     conn = psycopg2.connect(
-        dbname=DevelopmentConfig.DB_NAME,
-        user=DevelopmentConfig.DB_USERNAME,
-        password=DevelopmentConfig.DB_PASSWORD,
-        host=DevelopmentConfig.DB_HOST
+        dbname=Config.DB_NAME,
+        user=Config.DB_USERNAME,
+        password=Config.DB_PASSWORD,
+        host=Config.DB_HOST
     )
     return conn
